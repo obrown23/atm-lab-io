44280707cf0517c38f4fd43aef53b08b6248685f
KnightRider <omar.brown@comcast.net>
2019-04-08 14:01:30 -0400

    1st changes

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello from app.js")
+// console.log("Hello from app.js")
+// create buttons
+// listener for deposit button 
+// do not overdraft (balance will not reach negative balance)
+// input dollar amount 
+//color of bant account to reflect balance
+// accounts are independent of each other
+
+
+$('depositCheckingExchange').on("click", addMeduimOfExchange)
+$('depositCheckingExchange').on("click", addMeduimOfExchange)
+
+// listener for withdrawal button
+
+$('withdrawSavingExchange').on("click", minusMeduimOfExchange)
+$('withdrawSavingExchange').on("click", minusMeduimOfExchange)
+
